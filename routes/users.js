1e70b54c645c7bafc8993048549d6495630b75af
chris <cndruta@email.arizona.edu>
2018-10-30 13:10:43 -0700

    Added mongodb adapter and user model schema

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require("../models/users");
+var bcrypt = require("bcrypt-nodejs");
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.findOne({email: "coolguy12@fortnite.com"}, (err, user) => {
+    if (err)
+      res.status(500).json({error: err});
+    else if (user)
+      res.status(200).json(user);
+    else
+      res.status(200).json({error: "User not found"})
+  });
 });
 
 module.exports = router;
