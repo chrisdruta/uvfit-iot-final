86b09527ab747d4fd947bb47e144dd93ebc1239a
chris <cndruta@email.arizona.edu>
2018-10-31 18:11:50 -0700

    Added login endpoint for users, updated user model, installed jwt-simple

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,44 @@ var router = express.Router();
 
 var User = require("../models/users");
 var bcrypt = require("bcrypt-nodejs");
+var jwt = require("jwt-simple");
+
+var secret = "megachadz";
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   User.findOne({email: "coolguy12@fortnite.com"}, (err, user) => {
     if (err)
       res.status(500).json({error: err});
+
     else if (user)
       res.status(200).json(user);
+
     else
-      res.status(200).json({error: "User not found"})
+      res.status(200).json({error: "User not found"});
+  });
+});
+
+router.post('/login', (req, res, next) => {
+  User.findOne({email: req.body.email}, (err, user) => {
+    if (err)
+      res.status(401).json({success: false, error: err});
+
+    else if (user) {
+      bcrypt.compare(req.body.password, user.passwordHash, (err, isValid) => {
+        if (err)
+          res.status(401).json({success: false, error: err});
+
+        else if (isValid)
+          res.status(201).json({success: true, token: jwt.encode({email: user.email}, secret)});
+
+        else
+          res.status(401).json({success: false, error: "The email or password provided was invalid"});
+      });
+    }
+
+    else
+      res.status(401).json({success: false, error: "The email or password provided was invalid"});
   });
 });
 
