57ce2de7b21ec870d34a888a2b9b258edfb18c45
chris <cndruta@email.arizona.edu>
2018-11-04 21:17:32 -0700

    Added registration endpoint & added comments

--- routes/users.js
+++ routes/users.js
@@ -25,6 +25,15 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/login', (req, res) => {
+	/**
+	 * POST /user/login endpoint that checks user credentials and provides an auth token
+	 * 
+	 * Input:
+	 * 		JSON object containing items with keys: email, password
+	 * Output:
+	 * 		JSON containing jwt or error
+	 */
+	
 	User.findOne({email: req.body.email}, (err, user) => {
     	if (err)
       		res.status(401).json({success: false, error: err});
@@ -47,7 +56,55 @@ router.post('/login', (req, res) => {
 	});
 });
 
-router.post('/devices', (req, resp) => {
+router.post('/register', (req, resp) => {
+	/**
+	 * POST /user/register endpoint that creates a new user in the db
+	 * 
+	 * Input:
+	 * 		JSON object containing items with keys: email, name, password
+	 * Output:
+	 * 		JSON containing registration information or error
+	 */
+
+	if (!req.body.email || !req.body.name || !req.body.password)
+		return resp.status(400).json({success: false, error: "Missing new user registration info"})
+
+	// Regular expressions described and taken from https://gist.github.com/ravibharathii/3975295
+	const emailRe = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/;
+	const passRe = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+
+	if (emailRe.test(req.body.email) && passRe.test(req.body.password)) {
+		bcrypt.hash(req.body.password, null, null, (err, hash) => {
+			const newUser = new User({
+				email: req.body.email,
+				fullName: req.body.name,
+				passwordHash: hash
+			});
+
+			newUser.save((err, user) => {
+				if (err)
+					resp.status(400).json({success: false, error: err.errmsg});
+				else
+					resp.status(201).json({success: true, message: `User has been created for email ${user.email}`});
+			});
+		});
+	}
+
+	else
+		return resp.status(400).json({success: false, error: "Given information doens't pass regex"});
+
+});
+
+router.get('/devices', (req, resp) => {
+	/**
+	 * GET /user/devices endpoint that returns a JSON of user's device's information
+	 * 
+	 * Input:
+	 * 		Encoded JWT in header
+	 * Output:
+	 * 		JSON containing an array of user's device's information
+	 */
+	
 	if (!req.headers['x-auth'])
 		return resp.status(401).json({success: false, error: "Authentification parameter(s) missing"});
 
