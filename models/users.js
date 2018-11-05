ec0fe6cf0536d482c4b7e74d1f226d2df97a4679
chris <cndruta@email.arizona.edu>
2018-11-04 20:37:02 -0700

    Made white spacing consistent (tabs)

--- models/users.js
+++ models/users.js
@@ -1,10 +1,10 @@
 var db = require("../db");
 
 var userSchema = new db.Schema({
-    email: { type: String, required: true, unique: true},
-    fullName: { type: String, required: true},
-    passwordHash: String,
-    devices: [String] //TODO: Probably should change this array type from String to device schema 
+	email: { type: String, required: true, unique: true},
+	fullName: { type: String, required: true},
+	passwordHash: String,
+	devices: [String] //TODO: Probably should change this array type from String to device schema 
 });
 
 var User = db.model("User", userSchema);
