8ac9cee5734bd370e49886717cded24c87db2afe
chris <cndruta@email.arizona.edu>
2018-12-10 20:50:33 -0700

    yolo2

--- routes/users.js
+++ routes/users.js
@@ -165,7 +165,7 @@ router.put('/info', (req, res) => {
 					error: err
 				});
 			else if (user) {
-				bcrypt.compare(req.body.password, user.passwordHash, async (err, isValid) => {
+				bcrypt.compare(req.body.password, user.passwordHash, (async (err, isValid) => {
 					if (err)
 						res.status(401).json({
 							success: false,
@@ -263,7 +263,7 @@ router.put('/info', (req, res) => {
 							success: false,
 							error: "The password provided was invalid"
 						});
-				});
+				}));
 			} else {
 				res.status(401).json({
 					success: false,
