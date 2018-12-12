5b074beae4ccc0d22ba536d0c9a380076d6670cd
Danny <dkoehler@email.arizona.edu>
2018-12-11 19:13:46 -0700

    now its fixed

--- public/javascripts/activities.js
+++ public/javascripts/activities.js
@@ -69,7 +69,7 @@ function addToMap() {
     console.log(allActivities);
     console.log(this.attributes['name'].value);
     //for loop
-    for (var location of allActivities[this.name-1].route) {
+    for (var location of allActivities[this.attributes['name'].value-1].route) {
         var marker = L.marker([location.lat, location.long]).addTo(mymap);
     }
     document.getElementById("mapid").style.display = "block";
