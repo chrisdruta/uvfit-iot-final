acdc4e4e615719efa17e8af0b633df0452b432b8
Danny <dkoehler@email.arizona.edu>
2018-12-11 18:06:06 -0700

    li fixed

--- public/javascripts/activities.js
+++ public/javascripts/activities.js
@@ -39,11 +39,10 @@ function displayActivitiesList() {
     activityUl.innerHTML = "";
 
     for (var activity in allActivities) {
-        console.log("displayActivities for loop");
         var li = document.createElement("li");
         li.setAttribute('name', allActivities[activity].id);
         
-        li.innerHTML = "Activity " + allActivities[activity].id + "; " + allActivities[activity].type + "&emsp;Date: " + allActivities[activity].startDateTime.toLocaleString() + "&emsp;Duration: " + allActivities[activity].endDateTime - allActivities[activity].startDateTime / 60000 + "&emsp;Calories: " + allActivities[activity].caloriesBurned + "&emsp;UV: " + allActivities[activity].uvExposure;
+        li.innerHTML = "Activity " + (allActivities[activity].id) + "; " + (allActivities[activity].type) + "&emsp;Date: " + (allActivities[activity].startDateTime.toLocaleString()) + "&emsp;Duration: " + (allActivities[activity].endDateTime - (allActivities[activity].startDateTime / 60000)) + "&emsp;Calories: " + allActivities[activity].caloriesBurned + "&emsp;UV: " + allActivities[activity].uvExposure;
         li.addEventListener("click", addToMap);
         activityUl.appendChild(li);
         console.log(li);
