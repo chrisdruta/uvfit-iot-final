94fdda86a036cedc6f4d6d215f7a96b5f2de58d8
Danny <dkoehler@email.arizona.edu>
2018-12-11 19:45:25 -0700

    fix map

--- public/javascripts/activities.js
+++ public/javascripts/activities.js
@@ -60,17 +60,19 @@ function displayActivitiesList() {
 function addToMap() {
     document.getElementById("mapid").innerHTML = "";
 
-    mymap.remove();
-    if (mymap == null) {
-        mymap = L.map('mapid').setView([32.242725, -110.963965], 16);
+    if(mymap != null) {
+        mymap.remove();
+    }
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGtvZWhsZXIxNCIsImEiOiJjanBqNDFwcGwwMnZyM3Bwcm5jdmltN3Q3In0.ij9GLBpuJhRlvj-CLFyMjg', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: 'your.mapbox.access.token'
-        }).addTo(mymap);
-    } 
+    mymap = L.map('mapid').setView([32.242725, -110.963965], 16);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGtvZWhsZXIxNCIsImEiOiJjanBqNDFwcGwwMnZyM3Bwcm5jdmltN3Q3In0.ij9GLBpuJhRlvj-CLFyMjg', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+ 
     
     //for loop
     for (var location of allActivities[this.attributes['name'].value-1].route) {
